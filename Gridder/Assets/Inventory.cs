7886f8bae15805e2debb3e496880231488ec55b9
KarlRoe <karl.roe@playmind.com>
2015-09-02 09:18:39 -0400

    Grid on

---
+++ Gridder/Assets/Inventory.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject[] slots;
+	public GameObject currentSlot;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		for(int i = 0; i < 9; i++)
+		{
+			if(GetComponent<ItemSpawner>().ObjectInventory[i] <= 0)
+			{
+				slots[i].transform.GetChild (0).gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 0.25f);
+			}
+
+			else
+			{
+				slots[i].transform.GetChild (0).gameObject.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 1);
+			}
+		}
+
+		slots [9].transform.GetChild (0).gameObject.GetComponent<Text> ().text = GetComponent<ItemSpawner> ().ObjectInventory [GetComponent<ItemSpawner> ().objectIndex] + ".";
+
+//		for(int i = 0; i < 10; i++)
+//		{
+//			slots[i].SetActive (false);
+//		}
+		if(Input.GetButtonDown("Inventory"))
+		{
+
+			for(int i = 0; i < 10; i++)
+			{
+				if(!slots[i].activeSelf)
+					slots[i].SetActive (true);
+				else
+					slots[i].SetActive (false);
+			}
+		}
+		GetComponent<RectTransform> ().position = player.transform.position - new Vector3 (0,0,1);
+		currentSlot.transform.position =  slots[GetComponent<ItemSpawner>().objectIndex].transform.position;
+
+	}
+}
---
+++ Gridder/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	public bool onGround;
+	public LayerMask layM;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		onGround = false;
+
+		if(Physics2D.Raycast (new Vector2(transform.position.x, transform.position.y) + new Vector2(0.5f, 0), -Vector2.up, transform.localScale.y))
+		{
+			onGround = true;
+		}
+		if(Physics2D.Raycast (new Vector2(transform.position.x, transform.position.y) + new Vector2(-0.5f, 0), -Vector2.up, transform.localScale.y))
+		{
+			onGround = true;
+		}
+		if(Physics2D.Raycast (new Vector2(transform.position.x, transform.position.y), -Vector2.up, transform.localScale.y, layM))
+		{
+			onGround = true;
+		}
+
+	}
+}
